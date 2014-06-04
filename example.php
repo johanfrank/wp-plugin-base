2bef7101ec1524a0161ffc316aac6ecdeab0ea8f
Johan Norberg <johan.norberg@frojd.se>
2014-06-04 17:26:26 +0200

    added some features

--- example.php
+++ example.php
@@ -4,20 +4,84 @@
 * Plugin URI: https://github.com/pjnorberg/wp-plugin-base
 * Description: Example class for WPB
 * Version: 1.0
-* Author: Johan Norberg (pjnorberg)
+* Author: Johan Norberg (@pjnorberg)
+* Author URI: http://johannorberg.biz
 */
 
 include_once('wp-plugin-base.php');
 
 class ExamplePlugin extends WPB\Base {
 
+	// Set up a translate_domain if you want to use localization for your plugin:
+	protected $translate_domain = 'wpb_swedish';
+
 	public function __construct() {
 
-		$this->cpt = array(
-			'my_example_cpt'
+		// Set up project prefix used to name fields and variables:
+		$this->project_prefix = 'wpb';
+
+		// Define my custom post types:
+		$this->post_types = array(
+			'cpt_book' => array(
+				'label' => __('Book', $this->translate_domain),
+				'public' => true
+			),
+			'cpt_bookcase' => array(
+				'label' => __('Bookcase', $this->translate_domain),
+				'public' => true,
+			),
+		);
+
+		// Set up some post meta fields for our Books:
+		$this->metaboxes = array(
+			'mb_book_details' => array(
+				'metabox' => array(
+					'title' => __('Book details', $this->translate_domain),
+					'post_type' => 'cpt_book',
+					'priority' => 'high',
+					'context' => 'side'
+				),
+				'post_meta' => array(
+					'writer' => array(
+						'label' => __('Writer', $this->translate_domain),
+						'type' => 'text',
+						'before_render' => 'custom_before_render',
+						'before_save' => 'custom_before_save'
+					)
+				)
+			),
+			'mb_bookcase_list' => array(
+				'metabox' => array(
+					'title' => __('List of books', $this->translate_domain),
+					'post_type' => 'cpt_bookcase',
+					'context' => 'side',
+					'render' => 'custom_rendering_function'
+				),
+				'post_meta' => array(
+					'books' => array(
+						'label' => __('Books', $this->translate_domain),
+						'type' => 'email'
+					)
+				)
+			),
 		);
 
 		parent::__construct();
+	}
+
+	public function custom_rendering_function($meta_key, $data) {
+
+		return 'my own rendering function.';
+	}
+
+	public function custom_before_render($data) {
+
+		return $data;
+	}
+
+	public function custom_before_save($data) {
+
+		return $data;
 	}
 }
 
--- wp-plugin-base.php
+++ wp-plugin-base.php
@@ -2,12 +2,24 @@
 
 namespace WPB;
 
+/**
+ * wp-plugin-base
+ */
+
 class Base {
 
 	protected $plugin_base;
 	protected $plugin_rel_base;
-	protected $cpt;
+	protected $project_prefix;
+	protected $meta_key_postfix = 'meta_value_key';
 
+	protected $post_types;
+
+	/**
+	 * [__construct]
+	 * 
+	 * @param string $prefix - prefix for various plugin fields and values
+	 */
 	public function __construct() {
 
 		$this->plugin_base = rtrim(dirname(__FILE__), '/');
@@ -18,15 +30,15 @@ class Base {
 		register_uninstall_hook(__FILE__, array(get_class(), 'uninstall_hook'));
 
 		add_action('init', array($this, 'register_cpt'));
+		add_action('add_meta_boxes', array($this, 'register_metaboxes'));
+		add_action('save_post', array($this, 'save_post_meta'));
 	}
 
 	public function activation_hook($network_wide) {
-
 		$this->check_requirements();
 	}
 
 	public function deactivation_hook($network_wide) {
-
 	}
 
 	public static function uninstall_hook($network_wide) {
@@ -38,21 +50,122 @@ class Base {
 
 	public function register_cpt() {
 
-		echo '<pre>'.print_r($this->cpt, true).'</pre>';
+		if ($this->post_types) {
 
-		if ($this->cpt) {
-			foreach ($this->cpt as $post_type => $options) {
+			foreach ($this->post_types as $post_type => $options) {
 
-				if ($post_type == 0) {
-					$post_type = $options;
-					$options = null;
+				$post_type = ($post_type === 0 ? $options : $post_type );
+				$options = (!is_array($options) ? null : $options);
+
+				register_post_type($this->project_prefix . '_' . $post_type, $options);
+			}
+		}
+	}
+
+	public function register_metaboxes() {
+
+		if ($this->metaboxes) {
+
+			foreach ($this->metaboxes as $key => $mb) {
+
+				$context = (isset($mb['metabox']['context']) ? $mb['metabox']['context'] : 'advanced');
+				$priority = (isset($mb['metabox']['priority']) ? $mb['metabox']['priority'] : 'default');
+
+				add_meta_box(
+					$this->project_prefix . '_' . $key,
+					$mb['metabox']['title'],
+					array($this, 'register_metabox_callback'),
+					$this->project_prefix . '_' . $mb['metabox']['post_type'],
+					$context,
+					$priority
+				);
+			}
+		}
+	}
+
+	public function register_metabox_callback($post) {
+
+		wp_nonce_field($this->project_prefix . '_meta_box', $this->project_prefix . '_meta_box_nonce');
+
+		$this->load_post_meta($post->ID);
+
+		foreach ($this->metaboxes as $key => $mb) {
+			if ($post->post_type == $this->project_prefix . '_' . $mb['metabox']['post_type']) {
+				foreach ($mb['post_meta'] as $meta_key => $data) {
+					echo (isset($mb['metabox']['render']) ? $this->{$mb['metabox']['render']}($meta_key, $data) : $this->render($meta_key, $data));
 				}
+			}
+		}
+	}
 
-				if (empty($options)) {
-					$options = null;
+	public function render($meta_key, $data) {
+
+		$content = (isset($data['content']) ? $data['content'] : '');
+
+		$output = '<label>';
+
+		switch ($data['type']) {
+
+			case 'date':
+				$output .= $data['label'] . '<br><input name="'.$meta_key.'_meta_value_field" type="date" class="widefat" value="'.$content.'">';
+				break;
+
+			case 'email':
+				$output .= $data['label'] . '<br><input name="'.$meta_key.'_meta_value_field" type="email" class="widefat" value="'.$content.'">';
+				break;
+			
+			default:
+				$output .= $data['label'] . '<br><input name="'.$meta_key.'_meta_value_field" type="text" class="widefat" value="'.$content.'">';
+				break;
+		}
+
+		return $output .= '</label>';
+	}
+	
+	public function load_post_meta($post_id) {
+
+		foreach ($this->metaboxes as $mb_key => $mb) {
+			foreach ($mb['post_meta'] as $meta_key => $data) {
+
+				if (isset($data['before_render'])) {
+					$rendered = $this->{$data['before_render']}(get_post_meta($post_id, $this->project_prefix . '_' . $meta_key . '_' . $this->meta_key_postfix, true));
+					$this->metaboxes[$mb_key]['post_meta'][$meta_key]['content'] = $rendered;
+				} else {
+					$this->metaboxes[$mb_key]['post_meta'][$meta_key]['content'] = get_post_meta($post_id, $this->project_prefix . '_' . $meta_key . '_' . $this->meta_key_postfix, true);
 				}
+			}
+		}
+	}
 
-				register_post_type($post_type, $options);
+	public function save_post_meta($post_id) {
+
+		if (!isset( $_POST[$this->project_prefix . '_meta_box_nonce']))
+			return;
+
+		if (wp_verify_nonce($_POST[$this->project_prefix . '_meta_box'], $this->project_prefix . '_meta_box_nonce'))
+			return;
+
+		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+			return;
+
+		if (isset($_POST['post_type']) && 'page' == $_POST['post_type']) {
+			if (!current_user_can('edit_page', $post_id))
+				return;
+		} else {
+			if (!current_user_can('edit_post', $post_id))
+				return;
+		}
+
+		foreach ($this->metaboxes as $mb_key => $mb) {
+			foreach ($mb['post_meta'] as $meta_key => $data) {
+				if (isset($_POST[$meta_key . '_meta_value_field'])) {
+					if (isset($data['before_save'])) {
+						$finished = $this->{$data['before_save']}($_POST[$meta_key . '_meta_value_field']);
+						update_post_meta($post_id, $this->project_prefix . '_' . $meta_key . '_' . $this->meta_key_postfix, $finished);
+					} else {
+						update_post_meta($post_id, $this->project_prefix . '_' . $meta_key . '_' . $this->meta_key_postfix, $_POST[$meta_key . '_meta_value_field']);
+					}
+				}
 			}
 		}
 	}
