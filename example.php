47a0f3fa67de8bf202803dd7c6eff29b6dd95ec3
PJ Norberg <email@johannorberg>
2014-06-19 23:39:35 +0200

    changed indentation to 4 spaces

--- example.php
+++ example.php
@@ -12,143 +12,143 @@ include_once('wp-plugin-base.php');
 
 class ExamplePlugin extends WPB\Base {
 
-	// Set up a translate_domain if you want to use localization for your plugin:
+    // Set up a translate_domain if you want to use localization for your plugin:
 
-	protected $translate_domain = 'wpb_swedish';
+    protected $translate_domain = 'wpb_swedish';
 
-	public function __construct() {
+    public function __construct() {
 
-		// Set up project prefix used to name fields and variables:
+        // Set up project prefix used to name fields and variables:
 
-		$this->project_prefix = 'wpb';
+        $this->project_prefix = 'wpb';
 
-		// Define my custom post types:
+        // Define my custom post types:
 
-		$this->post_types = array(
-			'cpt_book' => array(
-				'label' => __('Book', $this->translate_domain),
-				'public' => true,
-				'rewrite' => array('slug' => 'book')
-			),
-			'cpt_bookcase' => array(
-				'label' => __('Bookcase', $this->translate_domain),
-				'public' => true,
-				'rewrite' => array('slug' => 'bookcase')
-			),
-		);
+        $this->post_types = array(
+            'cpt_book' => array(
+                'label' => __('Book', $this->translate_domain),
+                'public' => true,
+                'rewrite' => array('slug' => 'book')
+            ),
+            'cpt_bookcase' => array(
+                'label' => __('Bookcase', $this->translate_domain),
+                'public' => true,
+                'rewrite' => array('slug' => 'bookcase')
+            ),
+        );
 
-		// Set up some post meta fields for our Books:
+        // Set up some post meta fields for our Books:
 
-		$this->metaboxes = array(
-			'mb_book_details' => array(
-				'metabox' => array(
-					'title' => __('Book details', $this->translate_domain),
-					'post_type' => 'cpt_book',
-					'priority' => 'high',
-					'context' => 'side'
-				),
-				'post_meta' => array(
-					'writer' => array(
-						'label' => __('Writer', $this->translate_domain),
-						'type' => 'text',
-						'before_render' => 'custom_before_render',
-						'before_save' => 'custom_before_save'
-					),
-					'checkboxes' => array(
-						'label' => __('Checkboxes', $this->translate_domain),
-						'type' => 'checkbox',
-						'description' => __('Multiple selection is allowed.', $this->translate_domain),
-						'options' => array(
-							'sci-fi', 'horror', 'drama', 'thriller', 'comedy'
-						),
-					),
-					'radio' => array(
-						'label' => __('Radiobuttons', $this->translate_domain),
-						'type' => 'radio',
-						'description' => __('Only one selection is allowed.', $this->translate_domain),
-						'options' => array(
-							'banana', 'apple', 'lemon', 'orange', 'kiwi'
-						),
-					),
-					'color' => array(
-						'label' => __('Orwells favorite color', $this->translate_domain),
-						'type' => 'colorpicker',
-						'description' => __('Educated guess.', $this->translate_domain),
-					),
-					'second_color' => array(
-						'label' => __('Orwells second favorite color', $this->translate_domain),
-						'type' => 'colorpicker',
-						'description' => __('Just random!', $this->translate_domain),
-					),
-					'media' => array(
-						'label' => __('Image', $this->translate_domain),
-						'type' => 'media',
-						'description' => __('A picture or something else from the media gallery.', $this->translate_domain),
-					),
-				)
-			),
-			'mb_bookcase_list' => array(
-				'metabox' => array(
-					'title' => __('List of books', $this->translate_domain),
-					'post_type' => 'cpt_bookcase',
-					'context' => 'side',
-					'render' => 'custom_rendering_function'
-				),
-				'post_meta' => array(
-					'books' => array(
-						'label' => __('Books', $this->translate_domain),
-						'type' => 'email'
-					)
-				)
-			),
-		);
+        $this->metaboxes = array(
+            'mb_book_details' => array(
+                'metabox' => array(
+                    'title' => __('Book details', $this->translate_domain),
+                    'post_type' => 'cpt_book',
+                    'priority' => 'high',
+                    'context' => 'side'
+                ),
+                'post_meta' => array(
+                    'writer' => array(
+                        'label' => __('Writer', $this->translate_domain),
+                        'type' => 'text',
+                        'before_render' => 'custom_before_render',
+                        'before_save' => 'custom_before_save'
+                    ),
+                    'checkboxes' => array(
+                        'label' => __('Checkboxes', $this->translate_domain),
+                        'type' => 'checkbox',
+                        'description' => __('Multiple selection is allowed.', $this->translate_domain),
+                        'options' => array(
+                            'sci-fi', 'horror', 'drama', 'thriller', 'comedy'
+                        ),
+                    ),
+                    'radio' => array(
+                        'label' => __('Radiobuttons', $this->translate_domain),
+                        'type' => 'radio',
+                        'description' => __('Only one selection is allowed.', $this->translate_domain),
+                        'options' => array(
+                            'banana', 'apple', 'lemon', 'orange', 'kiwi'
+                        ),
+                    ),
+                    'color' => array(
+                        'label' => __('Orwells favorite color', $this->translate_domain),
+                        'type' => 'colorpicker',
+                        'description' => __('Educated guess.', $this->translate_domain),
+                    ),
+                    'second_color' => array(
+                        'label' => __('Orwells second favorite color', $this->translate_domain),
+                        'type' => 'colorpicker',
+                        'description' => __('Just random!', $this->translate_domain),
+                    ),
+                    'media' => array(
+                        'label' => __('Image', $this->translate_domain),
+                        'type' => 'media',
+                        'description' => __('A picture or something else from the media gallery.', $this->translate_domain),
+                    ),
+                )
+            ),
+            'mb_bookcase_list' => array(
+                'metabox' => array(
+                    'title' => __('List of books', $this->translate_domain),
+                    'post_type' => 'cpt_bookcase',
+                    'context' => 'side',
+                    'render' => 'custom_rendering_function'
+                ),
+                'post_meta' => array(
+                    'books' => array(
+                        'label' => __('Books', $this->translate_domain),
+                        'type' => 'email'
+                    )
+                )
+            ),
+        );
 
-		// Register your project CSS:
+        // Register your project CSS:
 
-		$this->stylesheets = array(
-			'style_example' => array(
-				'src' => '/css/example.css',
-				'deps' => array(),
-				'ver' => false,
-				'media' => 'all'
-			)
-		);
+        $this->stylesheets = array(
+            'style_example' => array(
+                'src' => '/css/example.css',
+                'deps' => array(),
+                'ver' => false,
+                'media' => 'all'
+            )
+        );
 
-		// Register your project JS:
+        // Register your project JS:
 
-		$this->scripts = array(
-			'script_example' => array(
-				'src' => '/js/example.js',
-				'deps' => array('jquery')
-			)
-		);
+        $this->scripts = array(
+            'script_example' => array(
+                'src' => '/js/example.js',
+                'deps' => array('jquery')
+            )
+        );
 
-		// Register your project taxonomies:
+        // Register your project taxonomies:
 
-		$this->taxonomies = array(
-			'tax_genre' => array(
-				'custom_post_types' => 'cpt_book',
-				'post_types' => array(),
-				'args' => array(
-					'label' => __('Genres', $this->translate_domain)
-				)
-			)
-		);
+        $this->taxonomies = array(
+            'tax_genre' => array(
+                'custom_post_types' => 'cpt_book',
+                'post_types' => array(),
+                'args' => array(
+                    'label' => __('Genres', $this->translate_domain)
+                )
+            )
+        );
 
-		parent::__construct(__FILE__);
-	}
+        parent::__construct(__FILE__);
+    }
 
-	public function custom_rendering_function($meta_key, $data) {
-		return '<label>'.$data['label'] . '<br><input name="'.$meta_key.'_meta_value_field" type="text" class="widefat" value="'.$data['content'].'">Old value: '.$data['content'].'</label>';
-	}
+    public function custom_rendering_function($meta_key, $data) {
+        return '<label>'.$data['label'] . '<br><input name="'.$meta_key.'_meta_value_field" type="text" class="widefat" value="'.$data['content'].'">Old value: '.$data['content'].'</label>';
+    }
 
-	public function custom_before_render($data) {
-		return $data;
-	}
+    public function custom_before_render($data) {
+        return $data;
+    }
 
-	public function custom_before_save($data) {
-		return $data;
-	}
+    public function custom_before_save($data) {
+        return $data;
+    }
 }
 
 $ExamplePlugin = new ExamplePlugin();
--- wp-plugin-base.js
+++ wp-plugin-base.js
@@ -1,52 +1,52 @@
 (function ($) {
 
-	$(document).ready(function () {
-		$('[data-field=colorpicker]').each(function() {
-			$(this).wpColorPicker();
-		});
-		$('[data-field=media]').each(function() {
-			handleImageUploads('#' + $(this).attr('id'));
-		});
-		$('[data-clear]').click(function() {
-			$('#' + $(this).attr('data-clear')).val('');
-		});
-	});
+    $(document).ready(function () {
+        $('[data-field=colorpicker]').each(function() {
+            $(this).wpColorPicker();
+        });
+        $('[data-field=media]').each(function() {
+            handleImageUploads('#' + $(this).attr('id'));
+        });
+        $('[data-clear]').click(function() {
+            $('#' + $(this).attr('data-clear')).val('');
+        });
+    });
 
-	/**
-	 * handleImageUploads - taps into WPs built-in media library overlay and lets the user select a custom image
-	 *
-	 * @params string css_identifier - trigger for overlay
-	 * @return void
-	 */
-	
-	function handleImageUploads(css_identifier) {
+    /**
+     * handleImageUploads - taps into WPs built-in media library overlay and lets the user select a custom image
+     *
+     * @params string css_identifier - trigger for overlay
+     * @return void
+     */
+    
+    function handleImageUploads(css_identifier) {
 
-		var _custom_media = true;
-		var _orig_send_attachment = wp.media.editor.send.attachment;
+        var _custom_media = true;
+        var _orig_send_attachment = wp.media.editor.send.attachment;
 
-		$(css_identifier).click(function(e) {
+        $(css_identifier).click(function(e) {
 
-			var send_attachment_bkp = wp.media.editor.send.attachment;
-			var button = $(this);
-			var id = button.attr('id').replace('_button', '');
-			_custom_media = true;
+            var send_attachment_bkp = wp.media.editor.send.attachment;
+            var button = $(this);
+            var id = button.attr('id').replace('_button', '');
+            _custom_media = true;
 
-			wp.media.editor.send.attachment = function(props, attachment) {
+            wp.media.editor.send.attachment = function(props, attachment) {
 
-				if (_custom_media) {
-					$("#"+id).val(attachment.url);
-				} else {
-					return _orig_send_attachment.apply(this, [props, attachment]);
-				};
-			}
+                if (_custom_media) {
+                    $("#"+id).val(attachment.url);
+                } else {
+                    return _orig_send_attachment.apply(this, [props, attachment]);
+                };
+            }
 
-			wp.media.editor.open(button);
-			return false;
-		});
+            wp.media.editor.open(button);
+            return false;
+        });
 
-		$('.add_media').on('click', function () {
-			_custom_media = false;
-		});		
-	}
+        $('.add_media').on('click', function () {
+            _custom_media = false;
+        });     
+    }
 
 }(jQuery));
